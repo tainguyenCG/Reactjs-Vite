ffcdf72821993c6bd9e0a0f6bdb7c82a70e64f25
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-06-28 02:15:03 +0700

    update

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,24 @@
+const TodoData = (props) => {
+    const { todoList, deleteTodo } = props;
+
+    const handleClick = (id) => {
+        deleteTodo(id)
+    }
+
+    return (
+        <div className='todo-data'>
+            {todoList.map((item, index) => {
+                return (
+                    <div className={`todo-item`} key={item.id}>
+                        <div> {item.name}</div>
+                        <button
+                            onClick={() => handleClick(item.id)}
+                            style={{ cursor: "pointer" }}>Delete</button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default TodoData;
